9ff76a422c2cf8d99420bd2a253a72e58dffdd7a
nmoralesj <nmoralesj@pandero.com.pe>
2023-06-30 20:46:55 -0500

    nuevos cambios en correo

--- swc/params/mail.js
+++ swc/params/mail.js
@@ -7,18 +7,12 @@ export const emailWelcome = {
     Body: `
  <body>
     <div class="container">
+        <p class="message">¡Bienvenido a la comunidad de SWC-P!</p>
         <img src="https://startwebperu.com/images/correo-swc.jpg" alt="Logo" class="logo">
-        <p class="message">¡Hola!</p>
+
         <p>Gracias por contactarnos. Pronto uno de nuestros trabajadores se comunicará contigo.</p>
         <hr class="hr">
-        <footer id="footer">
-            <ul class="icons">
-                <li><a href="#" style="text-decoration: none;"><i class="fab fa-twitter" style="color: #1da1f2;"></i><span class="label" style="font-size: 14px; color: #1da1f2; margin-left: 5px;">Twitter</span></a></li>
-                <li><a href="#" style="text-decoration: none;"><i class="fab fa-facebook-f" style="color: #1877f2;"></i><span class="label" style="font-size: 14px; color: #1877f2; margin-left: 5px;">Facebook</span></a></li>
-                <li><a href="#" style="text-decoration: none;"><i class="fab fa-instagram" style="color: #c32aa3;"></i><span class="label" style="font-size: 14px; color: #c32aa3; margin-left: 5px;">Instagram</span></a></li>
-                <li><a href="#" style="text-decoration: none;"><i class="fas fa-envelope" style="color: #000;"></i><span class="label" style="font-size: 14px; color: #000; margin-left: 5px;">Email</span></a></li>
-            </ul>
-        </footer>
+        
         <p class="footer">Este es un mensaje automático, por favor no respondas a este correo.</p>
     </div>
 </body>
