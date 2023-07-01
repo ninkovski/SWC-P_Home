c70ab1de80cdbad187c43784665ea8b10ef65d62
nmoralesj <nmoralesj@pandero.com.pe>
2023-07-01 10:26:14 -0500

    nuevos cambios en correo

--- swc/params/components.js
+++ swc/params/components.js
@@ -5,9 +5,18 @@
 
 export const redes = {
     twitter: '<li><a href="#" class="icon brands fa-twitter"><span class="label">Twitter</span></a></li>',
-    facebook: '<li><a href="#" class="icon brands fa-facebook-f"><span class="label">Facebook</span></a></li>',
-    instagram: '<li><a href="#" class="icon brands fa-instagram"><span class="label">Instagram</span></a></li>',
-    mail: '<li><a href="#" class="icon solid fa-envelope"><span class="label">Email</span></a></li>',
+    facebook:
+        '<li><a ' +
+        'href="https://www.facebook.com/profile.php?id=100094164543754"' +
+        'target="_blank" class="icon brands fa-facebook-f"><span class="label">Facebook</span></a></li>',
+    instagram:
+        '<li><a ' +
+        'href="https://www.instagram.com/start.web.peru/" ' +
+        'target="_blank"  class="icon brands fa-instagram"><span class="label">Instagram</span></a></li>',
+    whatsApp:
+        '<li><a ' +
+        'href="https://api.whatsapp.com/send?phone=51901976987&text=Hola%20me%20encantó%20tu%20pagina%20web%21" ' +
+        'target="_blank"  class="icon brands fa-whatsapp"><span class="label">Email</span></a></li>',
 };
 
 export const assets = {
@@ -19,7 +28,7 @@ export const redesGroup = `
     ${redes.twitter}
     ${redes.facebook}
     ${redes.instagram}
-    ${redes.mail} `;
+    ${redes.whatsApp} `;
 
 export const copyright =
     '<ul class="copyright">' +
