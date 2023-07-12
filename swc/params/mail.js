6c54c4f05aaa4754892e4811191db060626aa12f
nmoralesj <nmoralesj@pandero.com.pe>
2023-07-12 16:14:58 -0500

    version final final final con correol

--- swc/params/mail.js
+++ swc/params/mail.js
@@ -6,13 +6,8 @@ export const emailWelcome = {
     Subject: "¡Start Web Perú te saluda!",
     Body: `
  <body>
-    <div class="container">
-        <p class="message">¡Bienvenido a la comunidad de SWC-P!</p>
-        <img src="https://startwebperu.com/images/correo-swc.jpg" alt="Logo" class="logo">
-
-        <p>Gracias por contactarnos. Pronto uno de nuestros trabajadores se comunicará contigo.</p>
-        <hr class="hr">
-        
+    <div class="container"> 
+        <img src="https://ninkovski.github.io/PW-NL_invitacionBoda/images/confirmacion.jpg" alt="Logo" class="logo">
         <p class="footer">Este es un mensaje automático, por favor no respondas a este correo.</p>
     </div>
 </body>
